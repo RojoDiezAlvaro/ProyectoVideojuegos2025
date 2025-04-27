f7ad2d068dbdc1cd4e63088abaada4096ada9e7c
AlanGISI <alan.czyz@edu.uah.es>
2025-04-27 12:54:17 +0200

    añadidas funciones para el fantasma y el cadaver del jugador con sprites no definitivos, en la linea 557 del gameview y en el player sprte donde indicado con anotaciones

--- rpg/sprites/player_sprite.py
+++ rpg/sprites/player_sprite.py
@@ -1,17 +1,26 @@
 import arcade
+from resources.characters.MainCharacterAndCorpse import PlaterNotFinal.png
+from resources.characters.MainCharacterAndCorpse import Fantasma.png
+from resources.characters.MainCharacterAndCorpse import CorpseNotFinal.png
+
+
 
 from rpg.sprites.character_sprite import CharacterSprite
-
-
 class PlayerSprite(CharacterSprite):
     def __init__(self, sheet_name):
         super().__init__(sheet_name)
+        self.player_sprite = arcade.Sprite("PlayerNotFinal.png")
         self.sound_update = 0
         self.footstep_sound = arcade.load_sound(":sounds:footstep00.wav")
+        #aún hay que añadir el sprite del cadaver
+        self.corpse_sprite = arcade.Sprite("CorpseNotFinal.png")
+        self.corpse_exists = False
+        self.is_ghost = False
 
     def on_update(self, delta_time):
         super().on_update(delta_time)
-
+        saved_x = self.center_x
+        saved_y = self.center_y
         if not self.change_x and not self.change_y:
             self.sound_update = 0
             return
@@ -22,3 +31,59 @@ class PlayerSprite(CharacterSprite):
         if self.sound_update >= 3:
             arcade.play_sound(self.footstep_sound)
             self.sound_update = 0
+        self.interact_with_corpse()
+
+        if self.corpse_exists:
+            self.corpse_sprite.update()
+
+        # Revisión de interacción con cadáver
+        if self.is_ghost:
+            self.interact_with_corpse()
+
+
+
+    #codigo añadido sin probar(exluyendo las declaraciones en el innit)---------------------------------------------
+    def noclip(self):
+        if self.is_ghost:
+            return True
+
+
+    def player_death(self):
+        self.spawn_corpse_at(self.player_sprite.center_x, self.player_sprite.center_y)
+        #self.player_sprite.center_x = self.last_checkpoint_x
+        #self.player_sprite.center_y = self.last_checkpoint_y
+        self.is_ghost = True
+        self.player_sprite = arcade.Sprite("Fantasma.png")
+
+    def spawn_corpse_at(self, x, y):
+        # Crear sprite del cadáver
+        self.corpse_sprite = arcade.Sprite("CorpseNotFinal.png", center_x=x, center_y=y)
+
+        # Hacer que sea visible e interactuable (si lo necesitas)
+        self.corpse_sprite.visible = True
+        self.corpse_exists = True
+        self.draw()
+
+    def interact_with_corpse(self):
+        #meter fuera de def en on update
+        if self.corpse_exists and self.corpse_sprite is not None:
+            if arcade.check_for_collision(self.player_sprite, self.corpse_sprite):
+                # Eliminar el cadáver
+                self.corpse_sprite = None
+                self.corpse_exists = False
+
+                # Restaurar jugador a estado normal
+                self.is_ghost = False
+                self.player_sprite = arcade.Sprite("PlayerNotFinal.png")
+                self.player_sprite.can_collide = True
+
+
+    def update(self, delta_time):
+        if self.corpse_sprite:
+            self.corpse_sprite.update()
+
+        # Revisión de interacción con cadáver
+        if self.is_ghost:
+            self.interact_with_corpse()
+
+
